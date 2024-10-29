b0075809293d04599d051e21f608571360e9d94f
Edixon van Vliet <edixon.vanvliet@hotmail.com>
2024-10-29 18:53:13 -0400

    feat: Add credit limit to clients

--- backend/CuentasPorCobrar.API/Models/Database/Migrations/20241029225155_AddCreditLimit.Designer.cs
+++ backend/CuentasPorCobrar.API/Models/Database/Migrations/20241029225155_AddCreditLimit.Designer.cs
@@ -2,6 +2,7 @@
 using CuentasPorCobrar.Models.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CuentasPorCobrar.API.Models.Database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241029225155_AddCreditLimit")]
+    partial class AddCreditLimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
